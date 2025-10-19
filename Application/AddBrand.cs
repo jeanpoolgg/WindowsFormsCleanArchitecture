dff21e985f7926c336c17ec9c11b39ca7ad53aa9
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-19 14:00:17 -0500

    Capa Application

---
+++ Application/AddBrand.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+
+namespace Application
+{
+    public class AddBrand
+    {
+        private readonly IRepository<Brand> _repository;
+
+        public AddBrand(IRepository<Brand> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ExecuteAsync(Brand brand)
+        {
+            if(string.IsNullOrEmpty(brand.Name))
+            {
+                throw new ArgumentException("El nombre de la marca es obligatorio");
+            }
+
+            await _repository.AddAsync(brand);
+        }
+    }
+}
---
+++ Application/IRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Application
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T beer);
+    }
+}
