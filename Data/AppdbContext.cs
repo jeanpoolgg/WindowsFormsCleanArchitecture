11f8ba8a8a7f6ce1d0d1a7cb949ef78adbf60bae
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-25 15:49:20 -0500

    Capa Interface Adapters (Entity Framework)

---
+++ Data/AppdbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Especificar lo que tiene la base de datos
+        public DbSet<BrandModel> Brands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
