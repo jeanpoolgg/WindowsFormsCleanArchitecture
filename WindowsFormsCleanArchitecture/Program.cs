a8aaf048b6d8f6866fb2f45fe69c94e15cde88f2
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-25 18:16:13 -0500

    Inyeccion de Dependencias (Dependency Injection)

--- WindowsFormsCleanArchitecture/Program.cs
+++ WindowsFormsCleanArchitecture/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace WindowsFormsCleanArchitecture
 {
     internal static class Program
@@ -8,10 +10,25 @@ namespace WindowsFormsCleanArchitecture
         [STAThread]
         static void Main()
         {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FormMain());
+            // ApplicationConfiguration.Initialize();
+            // Application.Run(new FormMain());
+
+            var formMain = serviceProvider.GetRequiredService<FormMain>();
+            Application.Run(formMain);
+
+        }
+
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<FormMain>();
+            services.AddTransient<FormBrand>();
         }
     }
 }
