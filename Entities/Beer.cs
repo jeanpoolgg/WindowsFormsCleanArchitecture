8cd457c660cda77263f75c7830544b427cf0f2ee
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-28 20:00:41 -0500

    Creacion de Entity Beer

---
+++ Entities/Beer.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Entities
+{
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BrandId { get; set; }
+        public decimal Alcohol { get; set; }
+        public bool isStrongBeer() => Alcohol > 7.5m;
+    }
+}
