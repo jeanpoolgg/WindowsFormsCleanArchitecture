cda4fef93a597144184ee4a3c59351fc3d72aba5
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-28 20:06:49 -0500

    Caso de Uso Agregar Beer

---
+++ Application/AddBeer.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Entities;
+
+namespace ApplicationBusiness
+{
+    public class AddBeer
+    {
+        private readonly IRepository<Beer> _beerRepository;
+
+        public AddBeer(IRepository<Beer> beerRepository)
+        {
+            _beerRepository = beerRepository;
+        }
+
+        public async Task ExecuteAsync(Beer beer)
+        {
+            if(string.IsNullOrEmpty(beer.Name))
+            {
+                throw new ArgumentException("El nombre de la cerveza es obligatoria");
+            }
+
+            await _beerRepository.AddAsync(beer);
+        }
+    }
+}
