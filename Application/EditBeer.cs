97f446dd207d7b072b37c87d4fdd66e77b1fa6e5
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-10-28 20:13:57 -0500

    Caso de Uso Editar Beer

---
+++ Application/EditBeer.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Entities;
+
+namespace ApplicationBusiness
+{
+    public class EditBeer
+    {
+        private readonly IRepository<Beer> _repository;
+
+        public EditBeer (IRepository<Beer> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ExecuteAsync(Beer beer)
+        {
+            if(string.IsNullOrEmpty(beer.Name))
+            {
+                throw new ArgumentException("El nombre de la cerveza es obligatorio");
+            }
+
+            if(await _repository.GetByIdAsync(beer.Id) == null)
+            {
+                throw new ArgumentException("Cerveza no existe");
+            }
+
+            await _repository.EditAsync(beer);   
+        }
+
+    }
+}
